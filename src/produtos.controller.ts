c94e8a21c9e591c49236d723143c4d5dfb72946a
Ivan Ferreira <ivndev0@gmail.com>
2022-06-21 21:10:12 -0300

    Usando o CONSTRUCTOR

--- src/produtos.controller.ts
+++ src/produtos.controller.ts
@@ -1,8 +1,14 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { get } from "http";
-
+import { Produto } from "./produto.model";
 @Controller('produtos')
 export class ProdutosController{
+    produtos: Produto [] = [
+        new Produto ("LIV01", "Livro TDD e BDD", 43.65),
+        new Produto ("LIV02", "Mitologia", 93.34),
+        new Produto ("LIV03", "I.A como serviço", 63.65),
+    ];
+    }
     @Get()
     obterTodos():string {  // MÉTODO
         return 'Lista todos os produtos';
